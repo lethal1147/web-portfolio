dadea32c3004987679b3a3513836984d21d92e52
Joakim <yukimever@gmail.com>
2023-05-16 23:33:28 +0700

    fix:path

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,13 +3,6 @@ import SkillsSection from './component/skillsSection'
 import Navbar from './component/navbar'
 import ProjectsSection from './component/projectsSection'
 import Footer from './component/footer'
-import reactLogo from '/public/react-logo.png'
-import javaLogo from '/public/JavaScript-logo.png'
-import htmlLogo from '/public/html-logo.png'
-import cssLogo from '/public/css-logo.png'
-import nodejsLogo from '/public/nodejs-logo.png'
-import nextjsLogo from '/public/nextjs-logo.png'
-import typescriptLogo from '/public/Typescript_logo.png'
 
 export default function Home() {
   return (
