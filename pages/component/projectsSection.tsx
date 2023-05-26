ed72846c21bf680636bcd62521ce92e2615e91e0
Joakim <yukimever@gmail.com>
2023-05-26 15:07:18 +0700

    feat: add demo link to orangecat project

--- pages/component/projectsSection.tsx
+++ pages/component/projectsSection.tsx
@@ -10,7 +10,7 @@ interface project {
 }
 
 const projects: project[] = [
-    { id: 1, img: '/project-orangecat.png', name: "Orange Cat", description: "My final project at Generation bootcamp was an exercise tracking web-application", repo: 'https://github.com/JSD4-Orangecat/Activity_Tracking', demo: 'https://github.com/JSD4-Orangecat/Activity_Tracking' },
+    { id: 1, img: '/project-orangecat.png', name: "Orange Cat", description: "My final project at Generation bootcamp was an exercise tracking web-application", repo: 'https://github.com/JSD4-Orangecat/Activity_Tracking', demo: 'https://orangecat.vercel.app/' },
     { id: 2, img: '/project-findmyhat.png', name: "Find my hat", description: "I built a fun console game to find a hidden hat as part of my JavaScript assessment at Generation bootcamp", repo: 'https://replit.com/@yukimever/JSD4-FindMyHat-Assessment', demo: 'https://replit.com/@yukimever/JSD4-FindMyHat-Assessment' },
     { id: 3, img: '/project-omnifood.png', name: "Omnifood", description: "I designed a sleek landing-page for a mobile app in my HTML&CSS course on Udemy", repo: 'https://github.com/lethal1147/omnifood-HTMLandCSS', demo: 'https://omnifood-html-and-css.vercel.app/' },
     { id: 4, img: '/project-forkify.png', name: "Forkify", description: "My JavaScript course project on Udemy was a web-app that uses the Fetch API to find food recipes", repo: 'https://github.com/lethal1147/fokify-course', demo: 'https://fokify-course.vercel.app/' },
